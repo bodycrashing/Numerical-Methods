a159c0a95ea914257bc2651e80e45adb4fe11dc7
Hr. Madsen <asm.arbejde@gmail.com>
2018-06-22 12:34:20 +0200

    Final commit

--- Numerical Methods/Minimization/main.c
+++ Numerical Methods/Minimization/main.c
@@ -13,8 +13,7 @@ void vector_print(gsl_vector *v){
 	}
 
 
-
-double Rosenbrock(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
+double Rosenbrock_Hessian(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
   double x = gsl_vector_get(x0,0);
   double y = gsl_vector_get(x0,1);
   double fx = (1-x)*(1-x)+100*(y-x*x)*(y-x*x);
@@ -30,17 +29,14 @@ double Rosenbrock(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
 }
 
 
-
-double rosen(gsl_vector*v){
+double Rosenbrock(gsl_vector*v){
 	double x=gsl_vector_get(v,0);
 	double y=gsl_vector_get(v,1);
 	return pow(1-x,2)+100*pow(y-x*x,2);
 }
 
 
-
-
-double Himmelblau(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
+double Himmelblau_Hessian(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
   double x=gsl_vector_get(x0,0);
   double y=gsl_vector_get(x0,1);
   double fx = (x*x+y-11)*(x*x+y-11)+(x+y*y-7)*(x+y*y-7);
@@ -56,12 +52,11 @@ double Himmelblau(gsl_vector* x0, gsl_vector* grad, gsl_matrix* H){
 }
 
 
-double himmel(gsl_vector*v){
+double Himmelblau(gsl_vector*v){
 	double x=gsl_vector_get(v,0);
 	double y=gsl_vector_get(v,1);
 	return pow(x*x+y-11,2)+pow(x+y*y-7,2);
 }
-
 
 
 double fit_fun(gsl_vector* v){
@@ -73,7 +68,6 @@ double fit_fun(gsl_vector* v){
 	double A = gsl_vector_get(v,0);
 	double B = gsl_vector_get(v,1);
 	double T = gsl_vector_get(v,2);
-
 	double fx=0;
 	for(int i=0;i<N;i++){
 		fx += pow((A*exp(-t[i]/T)+B-y[i])/e[i],2);
@@ -82,138 +76,101 @@ double fit_fun(gsl_vector* v){
 }
 
 
-
-
-
-
-
-
 int main(int argc, char const *argv[]) {
-  printf("\n\n---------------------------------------------------------------------\n");
-  printf("---------------------------------------------------------------------\n");
-  printf("Minimisation for Rosenbrock's Function (1-x)^2+100(y-x^2)^2:\n");
-  printf("---------------------------------------------------------------------\n");
-  printf("---------------------------------------------------------------------\n");
+	/*--------------- Part A: Newton's minimization method---------------------*/
+	printf("\n|-------------- Part A: Newton's minimization method -------------------|\n");
+	double eps = 10e-3;
 
+	printf("Minimisation for Rosenbrock's Valley Function:\n");
   gsl_vector* x = gsl_vector_alloc(2);
   gsl_vector_set(x,0,2);
   gsl_vector_set(x,1,2);
   printf("Initial guess x0:\n");
   vector_print(x);
 
-double eps = 10e-3;
-int iter = newton_barebones(Rosenbrock, x, eps);
+	int iter_ros = newton_barebones(Rosenbrock_Hessian, x, eps);
+	printf("\n Number of steps = %i\n",iter_ros);
+	printf("Computed roots of Rosenbrok's Valley Function:\n");
+	vector_print(x);
 
-printf("\n Number of steps = %i\n",iter);
-printf("Computed roots of Rosenbrok's Valley Function:\n");
-vector_print(x);
-
-
-printf("\n\n---------------------------------------------------------------------\n");
-printf("---------------------------------------------------------------------\n");
-printf("Minimisation of Himmelblau's Function:\n");
-printf("---------------------------------------------------------------------\n");
-printf("---------------------------------------------------------------------\n");
-
-gsl_vector_set(x,0,-4);
-gsl_vector_set(x,1,-4);
-iter = newton_barebones(Himmelblau, x, eps);
-
-printf("\n Number of steps = %i\n",iter);
-printf("Computed roots of Himmelblau's Function:\n");
-vector_print(x);
+	printf("\n\nMinimisation of Himmelblau's Function:\n");
+	gsl_vector_set(x,0,-4);
+	gsl_vector_set(x,1,-4);
+	int iter_him = newton_barebones(Himmelblau_Hessian, x, eps);
+	printf("\n Number of steps = %i\n",iter_him);
+	printf("Computed roots of Himmelblau's Function:\n");
+	vector_print(x);
 
 
 
+	/*--------------- Part B: Quasi-Newton with Broyden's update -------------------*/
+	printf("\n\n|-------------- Part B: Quasi-Newton with Broyden's update -------------------|\n");
+	eps=10e-7;
+	double dx = 1e-7;
+	int nsteps;
 
+	// part ii.)
+	printf("\nQuasi-Newton: Rosenbrok's Function\n");
+	gsl_vector_set(x,0,2);
+	gsl_vector_set(x,1,2);
+	printf("Initial guess x0:\n");
+	vector_print(x);
+	nsteps = quasinewton(Rosenbrock,x,dx,eps);
+	printf("Steps=%i\n",nsteps);
+	printf("Roots:\n"); vector_print(x);
+	printf("f(x_root) = %lg\n", Rosenbrock(x));
 
-int nsteps;
-eps=10e-7;
-printf("eps=%.1e\n",eps);
-printf("\nMINIMIZATION OF ROSENBROCK'S FUNCTION\n");
-printf("start point:\n");
-gsl_vector_set(x,0,2);
-gsl_vector_set(x,1,2);
-double dx = 1e-7;
+	printf("\nQuasi-Newton: Himmelblau's Function\n");
+	gsl_vector_set(x,0,18);
+	gsl_vector_set(x,1,15);
+	printf("Initial guess x0:\n");
+	vector_print(x);
+	nsteps=quasinewton(Himmelblau,x,dx,eps);
+	printf("Steps=%i\n",nsteps);
+	printf("Roots:\n"); vector_print(x);
+	printf("f(x_root) = %lg\n", Himmelblau(x));
+	printf("\n\n");
 
-nsteps=quasinewton(rosen,x,dx,eps);
-printf("steps=%i\n",nsteps);
-vector_print(x);
+	// part iii.)
+	double t[] = {0.23,1.29,2.35,3.41,4.47,5.53,6.59,7.65,8.71,9.77};
+	double y[] = {4.64,3.38,3.01,2.55,2.29,1.67,1.59,1.69,1.38,1.46};
+	double e[] = {0.42,0.37,0.34,0.31,0.29,0.27,0.26,0.25,0.24,0.24};
+	int N = sizeof(t)/sizeof(t[0]);
 
+	FILE* data = fopen("fit_data.txt","w+"); // Save data to file "data"
+	for(int i=0;i<N;i++){
+	fprintf(data,"%lg\t%lg\t%lg\n",t[i],y[i],e[i]);
+	}
+	fprintf(data,"\n");
 
-gsl_vector_set(x,0,18);
-gsl_vector_set(x,1,15);
-nsteps=quasinewton(himmel,x,dx,eps);
-printf("steps=%i\n",nsteps);
-vector_print(x);
+	// Find constants of fit using Quasi_Newton minimization
+	dx=1e-6;
+	gsl_vector* x_fit = gsl_vector_alloc(3);
+	gsl_vector_set(x_fit,0,0.5);
+	gsl_vector_set(x_fit,1,1.0);
+	gsl_vector_set(x_fit,2,2.5);
 
+	printf("\n---- Expontial fit to supplied experimental data  ----\n");
+	printf("Initial guess of fitting coefficients:\n");
+	printf("A=%lg  B=%lg  T=%lg\n", gsl_vector_get(x_fit,0), gsl_vector_get(x_fit,1), gsl_vector_get(x_fit,2));
+	int step = quasinewton(fit_fun,x_fit,dx,eps);
+	printf("\nEstimate of fitting after Quasi-Newton Least Squares minimization:\n");
+	printf("A=%lg  B=%lg  T=%lg\n", gsl_vector_get(x_fit,0), gsl_vector_get(x_fit,1), gsl_vector_get(x_fit,2));
+	printf("Steps=%i \n\n",step);
 
+	// Print fit to data file
+	double t0=t[0], t_max = t[N-1], delta_t = t_max/200, A, B, T;
+	 A = gsl_vector_get(x_fit,0);
+	 B = gsl_vector_get(x_fit,1);
+	 T = gsl_vector_get(x_fit,2);
+	for(double t=t0; t<=t_max; t+=delta_t){
+	fprintf(data,"%lg\t%lg\n",t,A*exp(-t/T)+B);
+	}
+	fclose(data);
 
-
-
-
-
-
-double t[] = {0.23,1.29,2.35,3.41,4.47,5.53,6.59,7.65,8.71,9.77};
-double y[] = {4.64,3.38,3.01,2.55,2.29,1.67,1.59,1.69,1.38,1.46};
-double e[] = {0.42,0.37,0.34,0.31,0.29,0.27,0.26,0.25,0.24,0.24};
-int N = sizeof(t)/sizeof(t[0]);
-
-// save data in file
-FILE* data = fopen("fit_data.txt","w+");
-for(int i=0;i<N;i++){
-fprintf(data,"%lg\t%lg\t%lg\n",t[i],y[i],e[i]);
-}
-fprintf(data,"\n\n");
-
-// Find constants in minimization
-dx=1e-6;
-gsl_vector* x_fit = gsl_vector_alloc(3);
-gsl_vector_set(x_fit,0,0.5);
-gsl_vector_set(x_fit,1,1.0);
-gsl_vector_set(x_fit,2,2.5);
-
-printf("Fit to the given data with Broyden update and numerically calculated gradient given the start values\n");
-vector_print(x_fit);
-
-
-int step = quasinewton(fit_fun,x_fit,dx,eps);
-
-printf("is, with eps = %lg and step size = %lg\n",eps,dx);
-vector_print(x_fit);
-printf("with %i steps \n\n",step);
-
-// Print fit
-double t_0=t[0], t_max = t[N-1], delta_t = t_max/200,
- A = gsl_vector_get(x_fit,0), B=gsl_vector_get(x_fit,1),
- T = gsl_vector_get(x_fit,2);
-for(double t=t_0;t<=t_max;t+=delta_t){
-fprintf(data,"%lg\t%lg\n",t,A*exp(-t/T)+B);
-}
-fclose(data);
-
-// Free spcae
+// Free allocated memory
 gsl_vector_free(x);
 gsl_vector_free(x_fit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return 0;
 }
--- Numerical Methods/Minimization/minimization.c
+++ Numerical Methods/Minimization/minimization.c
@@ -4,14 +4,13 @@ int newton_barebones(double f(gsl_vector* x, gsl_vector* grad, gsl_matrix* H), g
 	int n = x0->size;
 	gsl_vector* grad = gsl_vector_alloc(n);
 	gsl_vector* z = gsl_vector_alloc(n);
-	//gsl_vector* fz = gsl_vector_alloc(n);
 	gsl_vector* Dx = gsl_vector_alloc(n);
   gsl_matrix* R = gsl_matrix_alloc(n,n);
   gsl_matrix* H = gsl_matrix_alloc(n,n);
 	int iter = 0;
+
 	while (1) {
 		iter++;
-
 		double fx = f(x0,grad,H); // the function "f" automatically fills the Hessian H and the
                   // the gradient of the given function when supllied with a vector
                   // of size(n) and a matirx of size(n,n).
@@ -40,21 +39,17 @@ int newton_barebones(double f(gsl_vector* x, gsl_vector* grad, gsl_matrix* H), g
 
 		}
 	gsl_vector_memcpy(x0,z);
-	//gsl_vector_memcpy(fx,fz);
 	if(gsl_blas_dnrm2(grad)<eps) break;
 	}
 
 	// Free allocated memory
 	gsl_matrix_free(H);
 	gsl_matrix_free(R);
-	//gsl_vector_free(fx);
 	gsl_vector_free(z);
 	gsl_vector_free(grad);
 	gsl_vector_free(Dx);
-	//gsl_vector_free(fz);
 	return iter;
 }
-
 
 
 
@@ -73,10 +68,7 @@ void numeric_grad(double f(gsl_vector* x), gsl_vector* x, gsl_vector* grad, doub
 
 
 
-
-
-int quasinewton(double f(gsl_vector* x),
-													gsl_vector* x0, double dx, double eps){
+int quasinewton(double f(gsl_vector* x), gsl_vector* x0, double dx, double eps){
 	int iter = 0 ;
 	int n = x0->size;
 
@@ -91,16 +83,16 @@ int quasinewton(double f(gsl_vector* x),
 
 	numeric_grad(f,x0,grad,dx);
 	double fx = f(x0);
+
 	while (iter<1000) {
 	iter++;
-
 	if(gsl_blas_dnrm2(grad)<eps){fprintf(stderr,"qnewton: |grad|<acc\n"); break;}
-
 	gsl_blas_dgemv(CblasNoTrans,-1,invH,grad,0,Dx);
 	double lambda = 1.0;
 	double alpha = 10e-5;
 	double sTgrad;
 	double fz;
+
 		while(1){
 			iter++;
 			gsl_vector_memcpy(z,x0);
@@ -112,7 +104,6 @@ int quasinewton(double f(gsl_vector* x),
 			lambda/=2.0;
 			gsl_vector_scale(Dx,lambda);
 		}
-
 		numeric_grad(f,z,gradz,dx);
 		gsl_vector_memcpy(y,gradz);
 		gsl_blas_daxpy(-1,grad,y); /* y = grad(z)-grad(x) */
@@ -120,14 +111,15 @@ int quasinewton(double f(gsl_vector* x),
 		gsl_blas_dgemv(CblasNoTrans,-1,invH,y,1,u); /* u=s-By */
 		double sTy,uTy;
 		gsl_blas_ddot(Dx,y,&sTy); // The denominator of eq.(14)
+
 		if(fabs(sTy)>1e-12){
 			gsl_blas_ddot(u,y,&uTy); //
 			double gamma=uTy/2/sTy;
 			gsl_blas_daxpy(-gamma,Dx,u); /* u=u-gamma*s */
-			// Add an vector to explicitly show what you are doing...
 			gsl_blas_dger(1.0/sTy,u,Dx,invH);
 			gsl_blas_dger(1.0/sTy,Dx,u,invH);
 		}
+
 		gsl_vector_memcpy(x0,z);
 		gsl_vector_memcpy(grad,gradz);
 		fx=fz;
@@ -142,5 +134,4 @@ gsl_vector_free(y);
 gsl_vector_free(u);
 
 	return iter;
-
 }
