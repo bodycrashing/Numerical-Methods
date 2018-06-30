f472342754bce224ebd9941650dc2ea95413f925
Hr. Madsen <asm.arbejde@gmail.com>
2018-06-30 02:28:37 +0200

    final commit

--- Numerical Methods/Symmetric Multi-Processing/main.c
+++ Numerical Methods/Symmetric Multi-Processing/main.c
@@ -18,6 +18,7 @@ int main(){
   gsl_vector* b = gsl_vector_alloc(dim);
   double exact = 1.3932039296856768591842462603255;
 
+  printf("----------- Plain Monte Carlo Integration WITHOUT Multi Threading -------------\n");
 	clock_t begin_time = clock();
   gsl_vector_set_zero(a);
   gsl_vector_set_all(b,M_PI);
@@ -26,31 +27,31 @@ int main(){
   printf("\nIntegration of gamma function from 0 to Pi:\n");
   printf("Error = %lg\n", err);
   printf("Result: Q=%lg\nCorrect result: Q_exact=%.15g\n",res,exact);
-
-
 	clock_t end_time = clock();
 	double time_spend = (double)(end_time-begin_time)/CLOCKS_PER_SEC;
-	printf("Time spend without Multi threading is %g seconds \n",time_spend);
+	printf("Time spend WITHOUT Multi threading: %g seconds \n",time_spend);
 
-printf("\n---------------------------------------------------------------------\n");
-printf("Test of Plain Monte Carlo Multi-Dimensional Integration With Multi Threading\n");
-printf("---------------------------------------------------------------------\n");
 
+  printf("\n----------- Plain Monte Carlo Integration WITH Multi Threading -------------\n");
 	begin_time = clock();
-
   gsl_vector_set_zero(a);
   gsl_vector_set_all(b,M_PI);
 
 	plain_monte_OMP(dim, a, b, f_gamma, N, &res, &err);
-
+  printf("\nIntegration of gamma function from 0 to Pi:\n");
+  printf("Error = %lg\n", err);
+  printf("Result: Q=%lg\nCorrect result: Q_exact=%.15g\n",res,exact);
   end_time = clock();
 	time_spend = (double)(end_time-begin_time)/CLOCKS_PER_SEC;
-	printf("\nTime spend with Multi threading is %g seconds \n",time_spend);
+	printf("Time spend WITH Multi threading: %g seconds \n",time_spend);
 
 
+  printf("\nAs is evident from the results the multi-threaded version of the plain\nMonte Carlo integration actually takes long time to");
+  printf("than the ordinary implementation.\nI have tested other students implentation on my Macbook and in all cases the multithreaded\n");
+  printf("are slower. In other words it seems that the time it takes to allocate the 'workers'\nis greater than time gained by actual multithreading.\n");
 
+  gsl_vector_free(a);
+  gsl_vector_free(b);
 
-gsl_vector_free(a);
-gsl_vector_free(b);
 return 0;
 }
